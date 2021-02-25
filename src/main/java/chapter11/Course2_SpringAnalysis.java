33b745935b1cb2854dee758df1f1a506bbdf714f
lijiajun <okjjlee@163.com>
2021-02-25 23:24:31 +0800

    【fix】优化一点文案

--- src/main/java/chapter11/Course2_SpringAnalysis.java
+++ src/main/java/chapter11/Course2_SpringAnalysis.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *      4、解析信息、支持国际化的能力，继承至MessageSource接口
  *      5、从父context继承。 在子context中的定义将始终优先。
  *
- *  三、ConfigurableApplicationContext：从ApplicationContext、LifeCycle继承。
+ *  三、ConfigurableApplicationContext：从ApplicationContext、LifeCycle扩展。
  *
  *  四、ResourceLoader：将给定的资源路径文件，解析成Resource对象
  *  Strategy interface for loading resources (e.. class path or file system resources).
@@ -23,12 +23,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *  ApplicationContext之外使用的独立实现，也可以由ResourceEditor使用。当在ApplicationContext中运行时，
  *  可以使用特定上下文的资源加载策略，从字符串填充类型为Resource和Resource array的Bean属性。
  *
- * 五、ResourcePatternResolver：扩展于ResourceLoader，解析本地pattern（）到Resource对象的策略接口。
+ * 五、ResourcePatternResolver：扩展于ResourceLoader，解析本地pattern（如xml）到Resource对象的策略接口。
  *
- * =============================================================================================
+ * 六、ApplicationEventPublisher：封装事件发布功能的接口。
  *
- * 六、ApplicationEventPublisher：封装事件发布功能的接口。用作ApplicationContext的超级接口。
  *
+ * ===============================================================================================
+ * 扩充知识：
+ * SPI： 全称为 (Service Provider Interface) ,是JDK内置的一种服务提供发现机制。
+ * 一个服务(Service)通常指的是已知的接口或者抽象类，服务提供方就是对这个接口或者抽象类的实现，
+ * 然后按照SPI 标准存放到资源路径META-INF/services目录下，文件的命名为该服务接口的全限定名。
+ *
+ * ===============================================================================================
  */
 
 public class Course2_SpringAnalysis {
