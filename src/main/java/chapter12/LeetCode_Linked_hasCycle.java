b908537a0c043013be44097ccc55c994ecbdeec5
lijiajun <okjjlee@163.com>
2021-03-07 17:25:52 +0800

    【add】快慢指针解决回环链表问题

---
+++ src/main/java/chapter12/LeetCode_Linked_hasCycle.java
@@ -0,0 +1,53 @@
+package chapter12;
+
+/**
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ */
+public class LeetCode_Linked_hasCycle {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
+        ListNode l9 = new ListNode(9);
+        ListNode l10 = new ListNode(10);
+
+//        l1.next = l2;
+//        l2.next = l3;
+//        l3.next = l4;
+//        l4.next = l5;
+//        l5.next = l6;
+//        l6.next = l7;
+//        l7.next = l8;
+//        l8.next = l9;
+//        l9.next = null;
+
+        System.out.println(hasCycle(l1));
+    }
+    public static boolean hasCycle(ListNode head) {
+        //1、使用容器存储每个结点
+        //todo
+        if (head == null) {
+            return false;
+        }
+        //2、使用双指针进行遍历
+        ListNode p1 = head, p2 = head;
+        while (p2.next != null && p2.next.next != null) {
+            p2 = p2.next.next;
+            p1 = p1.next;
+            if (p1 == p2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
