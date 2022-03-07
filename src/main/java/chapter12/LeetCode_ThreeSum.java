216aad9595d804181d9935f05ccb58561e5d2daa
luwu <luwu.lijj@menggejk.com>
2022-03-07 13:32:58 +0800

    +leetcode：算法优化

--- src/main/java/chapter12/LeetCode_ThreeSum.java
+++ src/main/java/chapter12/LeetCode_ThreeSum.java
@@ -25,11 +25,13 @@ public class LeetCode_ThreeSum {
 
 
     public static void main(String[] args) {
-        System.out.println(threeSum1(new int[]{1,2,-2,-1}));
+        System.out.println(threeSum(new int[]{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6}));
     }
 
     /**
      * 该算法超时。
+     *
+     * 后优化：利用数组有序的特性，不再全局遍历
      *
      * @param nums
      * @return
@@ -67,8 +69,10 @@ public class LeetCode_ThreeSum {
 
     /**
      * 利用数组有序的特性：
-     * 双指针思路：将问题转化成双数之和 - 某数为0的情况， 关键在于如何求两数之后。利用数组有序的特性，一数从左取，一数从右取，
-     * 双数相加，若
+     * 1、将三数之和转换为两数之和。
+     * 2、在判断两数之和时，因为数组有序，若第一数大于0，即可不用再判断两数之后了。
+     * 3、若两数相邻，则可以跳过两数之后的逻辑运算
+     * 4、双指针的思路：在求两数之和时，左指针往右移（右指针固定），若两数之和匹配，则记录，右指针则往左移一位（匹配不上右指针也往左移）
      *
      * @param nums
      * @return
@@ -76,12 +80,43 @@ public class LeetCode_ThreeSum {
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList();
-        Set<String> set = new HashSet<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length - 2 && nums[i] < 0; i++) {
+            while (nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2] && nums[i] < 0) {
+                i++;
+            }
 
+            int target = 0 - nums[i];
+            int lp = i + 1, rp = nums.length - 1;
+            while (lp < rp) {
+                if (target == nums[lp] + nums[rp]) {
+                    result.add(Arrays.asList(nums[i], nums[lp], nums[rp]));
+                    lp++;
+                    rp--;
+
+                    while (lp + 1 < nums.length && nums[lp] == nums[lp + 1] && nums[lp] < target) {
+                        lp ++;
+                    }
+                    while (nums[rp] == nums[rp - 1] && nums[rp] > target) {
+                        rp --;
+                    }
+
+                } else if (target < nums[lp] + nums[rp]) {
+                    rp--;
+                } else {
+                    lp++;
+                }
+            }
+
+            //跳一个过去
+            if (nums[i] == nums[i + 1] && nums[i] < 0) {
+                i++;
+            }
         }
 
+        if (Arrays.stream(nums).filter(t -> t == 0).count() >= 3) {
+            result.add(Arrays.asList(0, 0, 0));
+        }
         return result;
     }
 }
